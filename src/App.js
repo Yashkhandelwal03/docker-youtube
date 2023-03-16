d71fa435ccf81ba52616d84af19e8f8027bbe215
Gopal Khandelwal <yashkhandelwal003@gmail.com>
2023-03-16 07:35:28 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>An amazing startup idea !!.</p>
+        <p>What an amazing startup idea !!.</p>
         <p>Assignment done. LearnCodeOnline wooo</p>
         <p>Instagram: https://instagram.com/hiteshChoudharyOfficial</p>
       </header>
